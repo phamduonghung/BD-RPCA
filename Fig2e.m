f8403693309a179e29802fc6dad4daddd4f7cb8f
PHAM Duong Hung <phamduonghung1712@gmail.com>
2020-10-27 23:59:11 +0100

    Create Fig2e.m

---
+++ Fig2e.m
@@ -0,0 +1,21 @@
+%%% code matlab of Fig2e BD-RPCA%%%%%                                              ;
+clear  all;
+close all 
+%% Set Current Folder of MATLAB being BD-RPCA-GitHub and Add Path
+addpath(genpath(fullfile(pwd)));
+test=1;
+nomfichier='simu_conv' 
+result_folder = fullfile(pwd,'Results');
+mkdir(result_folder)
+%% Loading data
+iHS=0; % Not run Oleg
+load_data_US;
+%% ideal simulation figure
+FigFeatures.nomtest = 'ground_truth';
+IlogPWTD = -35*zeros(Nz,Nx,Nt);
+IlogPWTD(200:270,45:58,:)=1;
+IlogPWTD(145:180,90:100,:)=1;
+FigFeatures.color1='b';
+FigFeatures.color2= '--b';
+%FigFeatures.bar=1;
+[~,MIlogPWTD]=Dopplerplot(IlogPWTD,espace_xx,espace_zz,test,FigFeatures);                 
