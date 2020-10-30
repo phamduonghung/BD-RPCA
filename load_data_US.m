bd49b12753da07cb7182789915e0e964d7a629a6
PHAM Duong Hung <phamduonghung1712@gmail.com>
2020-10-30 10:59:18 +0100

    Update load_data_US.m

--- load_data_US.m
+++ load_data_US.m
@@ -24,45 +24,4 @@ if test ==1
     %     H = ones(Nz*Nx,Nt)+1i*ones(Nz*Nx,Nt);    
     espace_xx=1:Nx;
     espace_zz=1:Nz;
-else 
-    M1=real(double(IQ));
-    espace_xx=espace_x;
-    espace_zz=espace_z;
-    % Attribution de la taille de la matrice RF
-    [Nz,Nx,Nt] = size(M1); 
-    %%% INITIALISATION DE LA MATRICE PSF
-    if test ==2 
-        test
-        psf_fichier=fullfile(pwd,'Data','psf_cerveau.mat');
-        load(psf_fichier);                                %chargement de la matrice                             
-        psf = real(double(psf(:,:)));       
-        [m_hh,n_hh] = size(psf); 
-        shift_h = zeros(Nz*Nx, Nt);
-        shift_h(1:m_hh,1:n_hh) = psf;
-        H = fft2(circshift(shift_h, 1-[floor((m_hh+1)/2),floor((n_hh+1)/2)]));          
-    elseif test ==3
-        psf_fichier=fullfile(pwd,'Data','psf_peri.mat');
-        load(psf_fichier);                                %chargement de la matrice                             
-        psf = real(double(psf(:,:)));
-        [m_hh,n_hh] = size(psf); 
-        shift_h = zeros(Nz*Nx, Nt);
-        shift_h(1:m_hh,1:n_hh) = psf;
-        H = fft2( circshift( shift_h, 1-[floor((m_hh+1)/2),floor((n_hh+1)/2)] ) );
-    else
-        psf_fichier=fullfile(pwd,'Data','psf_tumeur.mat');
-        load(psf_fichier);                                %chargement de la matrice                             
-        apsf = real(double(IQ(:,:,1)));
-        [Pz,Px,Pt] = size(apsf); 
-        % choisir le point du milieu
-        m_h=32;
-        n_h=16;
-        point=[floor(Pz/2)-42 , floor(Px/2)+05];
-        psf = apsf(point(1)-floor(m_h/2):point(1)+floor(m_h/2) , point(2)-floor(n_h/2):point(2)+floor(n_h/2));
-        psf = psf/sum(psf(:));  
-        %    tic 
-        [m_hh,n_hh] = size(psf); 
-        shift_h = zeros(Nz*Nx, Nt);
-        shift_h(1:m_hh,1:n_hh) = psf;
-        H = fft2( circshift( shift_h, 1-[floor((m_hh+1)/2),floor((n_hh+1)/2)] ) );
-    end
 end
