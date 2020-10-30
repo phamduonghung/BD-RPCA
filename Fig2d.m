24dd86569f4f7649c0de79096b2568efd2691b51
PHAM Duong Hung <phamduonghung1712@gmail.com>
2020-10-30 09:19:01 +0100

    Update Fig2d.m

--- Fig2d.m
+++ Fig2d.m
@@ -49,8 +49,7 @@ err = zeros(1,max_iter);
 normM = norm(M, 'fro');
 
 for iter = 1:max_iter
-    iter
-    fprintf('Running estimated DRPCA for iteration %d....\n',iter)
+    fprintf('Running BDRPCA for iteration %d....\n',iter)
     [T, x] = DRPCA(M,H,Lambda1); % S <-> B (blood) and  L <->T (tissue) and M <-> S  and H<-> D in paper        
                    
     % Stop Condition
